12695ab70f1ae763b4443f6ee15ed3b9d3b26d21
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-01-08 13:40:27 +0530

    calculating the averate

---
+++ calAverage.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class calAverage{
+   public static void main(String [] args){
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter first number :");
+   double a = sc.nextDouble();
+   System.out.println("Enter second number :");
+   double b = sc.nextDouble();
+   System.out.println("Enter another number :");
+   double c = sc.nextDouble();
+   
+   
+   
+   
+   double ans=(a+b+c)/3;
+   System.out.println("Answer is : " + ans);
+   
+   }
+   
+  }
+   
