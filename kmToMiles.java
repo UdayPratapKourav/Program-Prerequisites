2fcc72950ff955dac5279196406d9488ecc7cf19
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-01-08 13:45:55 +0530

    calculating km to miles

---
+++ kmToMiles.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class kmToMiles{
+   public static void main(String [] args){
+   Scanner sc = new Scanner(System.in);
+   System.out.println("Enter value in kilometers :");
+   double a = sc.nextDouble();
+   
+   
+   
+   
+   
+   
+   System.out.println("value in miles is : " + ( a*0.621371));
+   
+   }
+   
+  }
+   
